f4892ab8a438ed715e6f3c982ef2edbcb87691e5
keroloussamy <keroloussamy98@gmail.com>
2023-09-16 13:30:06 +0300

    Add CategoryList design

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,18 @@
-import styles from './page.module.css'
+import CardList from "@/components/CardList/CardList";
+import CategoryList from "@/components/CategoryList/CategoryList";
+import Featured from "@/components/Featured/Featured";
+import Menu from "@/components/Menu/Menu";
+import styles from "./page.module.css";
 
 export default function Home() {
   return (
-    <main className={styles.main}>
-      
-    </main>
-  )
+    <div className={styles.container}>
+      <Featured />
+      <CategoryList />
+      <div className={styles.content}>
+        <CardList />
+        <Menu />
+      </div>
+    </div>
+  );
 }
--- src/components/CategoryList/CategoryList.tsx
+++ src/components/CategoryList/CategoryList.tsx
@@ -1,8 +1,73 @@
-import styles from './categoryList.module.css'
+import React from "react";
+import styles from "./categoryList.module.css";
+import Link from "next/link";
+import Image from "next/image";
 
-
-export default function CategoryList() {
+const CategoryList = async () => {
+  const data = [
+    {
+      _id: "1",
+      title: "Style",
+      slug: "style",
+      img: "/style.png",
+    },
+    {
+      _id: "2",
+      title: "Fashion",
+      slug: "fashion",
+      img: "/fashion.png",
+    },
+    {
+      _id: "3",
+      title: "Travel",
+      slug: "travel",
+      img: "/travel.png",
+    },
+    {
+      _id: "4",
+      title: "Food",
+      slug: "food",
+      img: "/food.png",
+    },
+    {
+      _id: "5",
+      title: "Culture",
+      slug: "culture",
+      img: "/culture.png",
+    },
+    {
+      _id: "6",
+      title: "Coding",
+      slug: "coding",
+      img: "/coding.png",
+    },
+  ];
+    
   return (
-    <div>CategoryList</div>
-  )
-}
+    <div className={styles.container}>
+      <h1 className={styles.title}>Popular Categories</h1>
+      <div className={styles.categories}>
+        {data?.map((item) => (
+          <Link
+            href="/blog?cat=style"
+            className={`${styles.category} ${styles[item.slug]}`}
+            key={item._id}
+          >
+            {item.img && (
+              <Image
+                src={item.img}
+                alt=""
+                width={32}
+                height={32}
+                className={styles.image}
+              />
+            )}
+            {item.title}
+          </Link>
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default CategoryList;
