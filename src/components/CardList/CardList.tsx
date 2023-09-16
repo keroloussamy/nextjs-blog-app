63f896cf69bd296929284493808ff0239c5fbe91
keroloussamy <keroloussamy98@gmail.com>
2023-09-16 20:00:56 +0300

    Add CardList and Card design

--- src/components/CardList/CardList.tsx
+++ src/components/CardList/CardList.tsx
@@ -1,8 +1,20 @@
-import styles from './cardList.module.css'
+import React from "react";
+import styles from "./cardList.module.css";
+import Card from "../Card/Card";
 
 
-export default function CardList() {
+const CardList = async () => {
   return (
-    <div>CardList</div>
-  )
-}
+    <div className={styles.container}>
+      <h1 className={styles.title}>Recent Posts</h1>
+      <div className={styles.posts}>
+          <Card />
+          <Card />
+          <Card />
+          <Card />
+      </div>
+    </div>
+  );
+};
+
+export default CardList;
