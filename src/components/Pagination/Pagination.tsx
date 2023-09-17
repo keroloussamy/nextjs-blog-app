6717ecf6d632e7483d4047d3a502adaf7b76a61d
keroloussamy <keroloussamy98@gmail.com>
2023-09-17 14:00:09 +0300

    Add Pagination component

--- src/components/Pagination/Pagination.tsx
+++ src/components/Pagination/Pagination.tsx
@@ -1,8 +1,32 @@
-import styles from './pagination.module.css'
+"use client";
 
+import React from "react";
+import styles from "./pagination.module.css";
+import { useRouter } from "next/navigation";
 
-export default function Pagination() {
+const Pagination = (
+  { page, hasPrev, hasNext } : { page: number, hasPrev: boolean, hasNext: boolean }
+  ) => {
+  const router = useRouter();
+
   return (
-    <div>Pagination</div>
-  )
-}
+    <div className={styles.container}>
+      <button
+        className={styles.button}
+        disabled={!hasPrev}
+        onClick={() => router.push(`?page=${page - 1}`)}
+      >
+        Previous
+      </button>
+      <button
+        disabled={!hasNext}
+        className={styles.button}
+        onClick={() => router.push(`?page=${page + 1}`)}
+      >
+        Next
+      </button>
+    </div>
+  );
+};
+
+export default Pagination;
