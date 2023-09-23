06c5e7a2a8207cbef9ac6b961daaae539579bc6a
keroloussamy <keroloussamy98@gmail.com>
2023-09-23 13:14:30 +0300

    Add Prisma schema and Prisma adapter

--- src/utils/auth.ts
+++ src/utils/auth.ts
@@ -1,8 +1,11 @@
+import { PrismaAdapter } from "@auth/prisma-adapter";
 import GithubProvider, { GithubProfile } from "next-auth/providers/github";
 import GoogleProvider, { GoogleProfile } from "next-auth/providers/google";
 import { OAuthUserConfig } from "next-auth/providers/oauth";
+import prisma from "./connect";
 
 export const authOptions = {
+  adapter: PrismaAdapter(prisma),
   providers: [
     GoogleProvider({
       clientId: process.env.GOOGLE_CLIENT_ID,
